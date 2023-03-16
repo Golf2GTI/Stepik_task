ad827de3429b1458886fd0ac824d2cd450464e91
Golf2GTI <Clackyll@yandex.ru>
2023-03-16 22:59:13 +0300

    Tests

--- conftest.py
+++ conftest.py
@@ -12,50 +12,6 @@ def browser(request):
     user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = None
-    if user_language == "ar":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "ca":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "cs":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "da":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "de":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "en-gb":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "el":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "es":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "fi":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "fr":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "it":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "ko":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "nl":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "pl":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "pt":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "pt-br":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "ro":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "ru":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "sk":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "uk":
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "zh-hans":
-        browser = webdriver.Chrome(options=options)
-    else:
-        raise pytest.UsageError("--language should be not null")
+    browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
